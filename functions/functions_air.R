b40acd70a4f95b18ce011e4a47cac0aa6034b978
Duret Steven <steven.duret@irstea.priv>
2021-11-26 16:55:36 +0100

    Implementation de la méthodologie de calcul du critère pour la classiffication en aérosol ou non.
    Calcul à vérifier, il semblait y avoir un problème mais je ne suis pas sur, je verrai la semaine prochaine.

--- functions/functions_air.R
+++ functions/functions_air.R
@@ -15,9 +15,14 @@ f_initAir <- function(
   ##  - NDays, ## total number of days during the entire process
   ##  - Step ## time step (in minutes)
   prm_air ## (list) The parameter associated with the air parameters
+  ##### OUTPUT 
+  # Evolution of the concentration of droplets classes over time in every room 
+  # MyAir (list)  rows -> Air_ID, it corresponds to one air agent for each room
+  #               Colums ->  drolet classes (depends on droplet size)
+  #               time   -> time ind : time index
 
 ){ 
-prm = Parms_Plant
+# prm = Parms_Plant
 # prm_time = Parms_Time
 # prm_air = Parms_Air
 
@@ -31,13 +36,14 @@ prm = Parms_Plant
   ##############################################################################
   ### ID of each rooms
   # AIR_ID <- names(prm$Spaces)
-  AIR_ID <- unname(unlist(lapply(prm$Spaces, function (x) return(x$label))))
+  AIR_ID <- c(prm$label,
+              unname(unlist(lapply(prm$Spaces, function (x) return(x$label)))))
   
   ### Droplet  classes d
   AIR_dclass <- paste("d", stringr::str_pad(seq(1:length(prm_air$Droplet_class)), width=2, pad="0"), sep="")
   ### Total number of the time indexes 
   NTime <- prm_time$NDays * 1440 / prm_time$Step ## amplitude Ndays in days, time step in minutes
-  t_ind <- rep(0:NTime, each = length(prm$Spaces))
+  t_ind <- rep(0:NTime, each = length(prm$Spaces)+1) # +1 for the cutting room not included as 'Spaces'
   
   ### Output
   AIR <- data.frame(AIR_ID = rep(AIR_ID, NTime+1),
@@ -49,3 +55,70 @@ prm = Parms_Plant
   #### END OF FUNCTION
 
 }
+
+f_Vsed <- function(
+#############################################################################
+# Calculates the sedimentation velocity of a sphere of water in the air
+# According to Stokes law
+# INPUT
+  prm_air #(list) Parameters associated with the air characteristics
+  # OUtput
+  #  Vsed (m.s-1) (value, vector) sedimentation velocity for each droplet class
+  ){
+  # prm = Parms_Air  
+  
+  g = 9.81 # (m².S-1) -  Gravity
+  mu = 1.76*10^(-5) # (Pas.s) - Dynamic viscosity at 10°C
+  rho_eau = 1000 # (kg.m-3) - Water density
+  rho_air = 1.25 # (kg.m-3) -Water density 
+
+return(2*(prm_air$Droplet_class*10^(-6))^2*g*(rho_eau-rho_air)/(9*mu)) # Stokes'law
+#### END OF FUNCTION
+}
+
+f_sed_time <-function(prm_plant, prm_air)
+  {
+  # prm_air = Parms_Air
+  # prm_plant = Parms_Plant
+  # Sedimentation Velocity
+  Vsed <- matrix(f_Vsed(prm_air),nrow=1) # (m.s-1) 
+  # Height of each room 
+  H_room <- matrix(c(prm_plant$dim.Z,
+                    unname(unlist(lapply(prm_plant$Spaces, function (x) return(x$dim.Z))))))
+   
+  return(aperm(H_room%*%(Vsed^-1)))
+}
+
+f_circ_time <- function (prm_plant){
+  V_rooms <- c(prm_plant$dim.X*prm_plant$dim.Y*prm_plant$dim.Z,
+              unname(unlist(lapply(prm_plant$Spaces, function (x) return(x$dim.X*x$dim.Y*x$dim.Z)))))
+  AirflowRate <- c(prm_plant$AirflowRate,
+              unname(unlist(lapply(prm_plant$Spaces, function (x) return(x$AirflowRate)))))
+    return(V_rooms/(AirflowRate/3600))
+  }
+
+f_Air_Criteria_Calc <-function(prm_plant, prm_air)
+  # Calculations based on Kenedy et al (2020)
+  # prm_air = Parms_Air
+  # prm_plant = Parms_Plant
+  {
+  # call function for sedimentation time calculation
+  sed_time <- f_sed_time(prm_plant,prm_air)  ## en sec ????????????
+  # Call function for circulation time calculation
+  circ_time <- f_circ_time(prm_plant) ## en heure ????????????
+  
+  return(circ_time/sed_time<0.5)
+}
+ 
+f_drop_conc_evol <- function (prm_plant,prm_air) # ... to be continued ... 
+{  
+  
+  Method_calc <- f_Air_Criteria_Calc(Parms_Plant,Parms_Air)
+  
+  
+  
+Method <- c(prm_plant$Air_Cond_Type,
+            unname(unlist(lapply(prm_plant$Spaces, function (x) return(x$Air_Cond_Type)))))}
+
+
+
