d4af5d62282fd7b62d18900b5dcc8bb95db892f2
Ngoc-Du Martin Luong <ngoc-du.luong@anses.fr>
2021-12-02 09:25:51 +0100

    NDL-2021-12-01-Modification-Parms-Workers//functions f_convertTime

--- functions/functions_workers.R
+++ functions/functions_workers.R
@@ -11,13 +11,8 @@ f_initWorkers <- function(
   #### INPUT
   prm, ## (list) the parameters associated with all attributes of the workers with at least the following elements
   ## (check the script "parameters_workers.R")
-  ##  - $NWorkers (integer), ## total number of the workers during the entire process
-  ##  - $InfectProb (numeric), ## probability of infected people among the workers
-  ##  - $MaskWearingProb (numeric) ## probability of people wearing a mask
   prm_time, ## (list) the parameters associated with the timetable
   ## (check the script "parameters_time.R")
-  ##  - NDays, ## total number of days during the entire process
-  ##  - Step ## time step (in minutes)
   ...
   #### OUTPUT
   ## W (dataframe): the initialised workers
@@ -36,7 +31,6 @@ f_initWorkers <- function(
   NTime <- prm_time$NDays * 1440 / prm_time$Step ## amplitude Ndays in days, time step in minutes
   t_ind <- rep(0:NTime, each = prm$NWorkers)
   
-  
   ### States (infected/not infected) of the workers
   # Time index 0:
   # HYPOTHESE : ONE INDIVIDUAL CONTAMINATED AT DAY 0 ?
@@ -46,22 +40,25 @@ f_initWorkers <- function(
   W_status <- as.factor(c(W_status0, rep(NA, prm$NWorkers * NTime)))
   
   ### Behavior (mask/no mask) of the workers
-  # Time index 0
-  W_mask0 <- sample(names(prm$pMask),
-                    size = prm$NWorkers, replace = T,
-                    prob = prm$pMask)
+  # At the time index 0
+  # The proportion of workers wearing a mask depending on the type of mask
+  pMask <- prm$pMaskAcceptability[prm$MaskType] %>% unname
+  
+  # Wearing mask or not for each worker
+  W_mask0 <- sample(c("mask", "no mask"), replace = T,
+                    size = prm$NWorkers,
+                    prob = c(pMask,1-pMask))
   # Initialization for the subsequent time indexes as NA
   W_mask <- as.factor(c(W_mask0, rep(NA, prm$NWorkers * NTime)))
-  
   
   ### Workers with a fixed or mobile work space
   # Time index 0
   W_type0 <- sample(names(prm$pType),
                     size = prm$NWorkers, replace = T,
                     prob = prm$pType)
-  # Initialization for the subsequent time indexes as NA
-  W_type <- as.factor(c(W_type0, rep(NA, prm$NWorkers*NTime)))
-  
+  # Replicate the type of workers for all subsequent time indexes
+  #W_type <- as.factor(c(W_type0, rep(NA, prm$NWorkers*NTime)))
+  W_type <- as.factor(rep(W_type0, NTime+1)) ## + 1 (including the time index 0)
   
   ### Initialize coordinates and location of the workers (NA)
   W_coordX <- rep(NA, prm$NWorkers*(NTime+1))
@@ -69,34 +66,15 @@ f_initWorkers <- function(
   W_location <- rep(NA, prm$NWorkers*(NTime+1))
   
   ### Initialize the working (or not-working phase) of the workers (NA)
-  # Time index 0
-  W_active0 <- sample(names(prm$pActive),
-                      size = prm$NWorkers, replace = T,
-                      prob = prm$pActive)
-  # Initialization for the subsequent time indexes as NA
-  W_active <- as.factor(c(W_active0, rep(NA, prm$NWorkers*NTime)))
+  W_active <- rep(NA, prm$NWorkers*(NTime+1))
+  W_activeCounter <- rep(NA, prm$NWorkers*(NTime+1))
   
-  ### Community activities
-  # The workers with/without community activities 
-  W_communityActivity <- sample(x = c(T,F),
-                                size = prm$NWorkers, replace = T,
-                                prob = c(prm$pCommunityActivity, 1-prm$pCommunityActivity))
+  ## Community activities
   ## Assign the workers to the different communes if applicable
-  # Total number of communes (groups) = round (total number with activities divided by the average number of workers per commune)
-  NCommunes <- round(length(which(W_communityActivity)) / prm$N_perCommunityActivity)
-  # Initialize the communes names 
-  Communes <- paste("Com", stringr::str_pad(seq(1:NCommunes), width=3, pad="0"), sep="")
+  AssignedCommunity <- f_assignCommunes(prm) ## check the (sub)function f_assignCommunes()
   
-  ## Communes of each worker
-  W_communes <- rep(NA, prm$NWorkers)
-  workers_index <- sample(which(W_communityActivity)) ## randomize the indexes of the workers with community activities
-  
-  for (ic in 1:NCommunes) {
-    ind_begin <- ic * prm$N_perCommunityActivity - (prm$N_perCommunityActivity - 1)
-    ind_end <- ic * prm$N_perCommunityActivity
-    W_communes[workers_index[ind_begin:ind_end]] <- Communes[ic]
-  }
-
+  W_communityActivity <- AssignedCommunity$W_communityActivity0 %>% rep(NTime+1) 
+  W_communes <- AssignedCommunity$W_communes0 %>% rep(NTime+1) %>% as.factor
   
   ### Output
   W <- data.frame(W_ID = rep(W_ID, NTime+1),
@@ -104,8 +82,9 @@ f_initWorkers <- function(
                   W_mask = W_mask,
                   W_type = W_type,
                   W_active = W_active,
-                  W_communityActivity = rep(W_communityActivity, NTime+1),
-                  W_communes = rep(W_communes, NTime+1),
+                  W_activeCounter = W_activeCounter,
+                  W_communityActivity = W_communityActivity,
+                  W_communes = W_communes,
                   W_coordX = W_coordX,
                   W_coordY = W_coordY,
                   W_location = W_location,
@@ -113,5 +92,60 @@ f_initWorkers <- function(
   )
   
   return(W)
+  #### END OF FUNCTION
+}
+
+##### f_assignCommunes() SUB-FUNCTION TO ASSIGN WORKERS TO DIFFERENT COMMUNES #####
+f_assignCommunes <- function(
+  prm ## (list) the parameters associated with all attributes of the workers with at least the following elements
+  ## (check the script "parameters_workers.R")
+) {
+  #### BEGIN OF FUNCTION
+  ### Community activities
+  # The workers with/without community activities 
+  W_communityActivity0 <- sample(x = c(T,F),
+                                 size = prm$NWorkers, replace = T,
+                                 prob = c(prm$pCommunityActivity, 1-prm$pCommunityActivity))
+  
+  ## Assign the workers to the different communes if applicable
+  # Total number of communes (groups) depending on the total number with activities and the average number of workers per commune
+  # The number of communes
+  NfullCommunes <- length(which(W_communityActivity0)) %/% prm$N_perCommunityActivity
+  leftover <- length(which(W_communityActivity0)) %% prm$N_perCommunityActivity
+  
+  if (leftover <= 1) {
+    LastCommunes <- NfullCommunes
+  } else {
+    LastCommunes <- NfullCommunes + 1
+  }
+  
+  # Initialize the communes names 
+  Communes0 <- paste("Com", stringr::str_pad(seq(1:LastCommunes), width=3, pad="0"), sep="")
+  
+  ## Communes of each worker
+  W_communes0 <- rep(NA, prm$NWorkers)
+  workers_index <- sample(which(W_communityActivity0)) ## randomize the indexes of the workers with community activities
+  
+  for (ic in 1:NfullCommunes) {
+    ind_begin <- ic * prm$N_perCommunityActivity - (prm$N_perCommunityActivity - 1)
+    ind_end <- ic * prm$N_perCommunityActivity
+    W_communes0[workers_index[ind_begin:ind_end]] <- Communes0[ic]
+  }
+  
+  if (LastCommunes > NfullCommunes) { ## s'il reste au moins 2 personnes en trop
+    ind_begin <- LastCommunes * prm$N_perCommunityActivity - (prm$N_perCommunityActivity - 1)
+    ind_end <- ind_begin + leftover - 1
+    W_communes0[workers_index[ind_begin:ind_end]] <- Communes0[LastCommunes]
+  } else {
+    if (leftover == 1) {
+      LastWorkers <- LastCommunes * prm$N_perCommunityActivity + 1
+      W_communes0[workers_index[LastWorkers]] <- Communes0[LastCommunes]
+    }
+  }
+  
+  AssignedCommunity <- data.frame(W_communityActivity0 = W_communityActivity0,
+                                  W_communes0 = W_communes0)
+  
+  return(AssignedCommunity)
   #### END OF FUNCTION
 }
--- parameters/parameters_workers.R
+++ parameters/parameters_workers.R
@@ -1,35 +1,54 @@
 ##### USER DEFINED PARAMETERS FOR THE WORKERS SET #####
 Parms_Workers <- list(
-  NWorkers = 200, ## total number of the workers during the entire process
-  nContaminated_Init = 1,  ## the number of contaminated workers at the day 0
-  # pContaminated = c("contaminated" = 0.1,
-  #             "not contaminated" = 0.9), ## probability of infected people among the workers
-  pMask = c("mask" = 0.80,
-            "no mask" = 0.20), ## probability of workers wearing a mask
-  Mask_Eff = c(0.9,  # Mask Efficacy for each droplet class [0 -> 1]
-               0.9, 
-               0.9, 
-               0.9),
-  pActive = list(
-    W_gr1 = c("idle", "active", "active", "active", "active"),
-    W_gr2 = c("active", ),
-    W_gr3 = c(),
-    W_gr4 = c(),
-    W_gr5 = c()
-  ),
-  pChangeTeam = 0.05,
-  # pActive = c("active" = 0.8,
-  #             "idle" = 0.2), ## proportion of active workers (per day) upon the total number of employees
+  #### NUMBER OF WORKERS ####
+  ## Total number of fixed workers (regular/temporary/subcontract)
+  NWorkers = 100, 
+  ## Total number of exterior workers
+  NWorkers_ext = 15,
+  
+  #### TYPE OF WORKERS - TEAMS - SHIFT ####
+  ## The ratio between the total number of workers over the required number of workers per working day
+  r_Nt_Nperday = 1.25, ## French legislation
+  ## Overall proportion for different types of workers
   pType = c("cutter1" = 0.36, ## Mallet et al. 2021
             "cutter2" = 0.47, ## Mallet et al. 2021 
             "logistic1" = 0.04, ## Gunther et al. 2020
             "logistic2" = 0.04, ## Mallet et al. 2021 
-            "transverse" = 0.09),
+            "transverse1" = 0.045, ## Mallet et al. 2021 (total number of transverse workers = 0.09)
+            "transverse2" = 0.045), ## Mallet et al. 2021 (total number of transverse workers = 0.09)
+  ## The proportion of workers (cutters/logistics) in two teams A and B
+  ## which alternatively change the working shift (morning/afternoon) from one week to another 
   pTeam = c("TeamA" = 0.5, 
-            "TeamB" = 0.5), 
-  mandatoryWorkersType = c("transverse"),
-  prev = 50/100000, ## regional prevalence
-  pCommunityActivity = 0.30, ## the proportion of the workers susceptible to have community activities
-  N_perCommunityActivity = 3, ## the average number of workers within a group for activity
-  conta_prob_withinCommunity = 0.025 ## CHECK LITERATURE (25% every 10 days)
+            "TeamB" = 0.5),
+  ## Proportion of workers (cutters/logistics) susceptible to change the teams (A/B) for a given week
+  pChangeTeam = 0.05,
+  ## Worker types with important mission having high probabilities to be present at work 
+  ## despite their possible contamination/symptoms
+  mandatoryWorkersType = c("transverse1"),
+  
+  #### COMMUNITY ACTIVITIES ####
+  ## Proportion of the workers susceptible to have community activities
+  pCommunityActivity = 0.30, 
+  ## Average number of workers within a commune for activity
+  N_perCommunityActivity = 3, 
+  ## Probability of contamination within a commune for activity
+  conta_prob_withinCommunity = 0.025, ## CHECK LITERATURE DATA (average daily value of 25% every 10 days)
+  
+  #### MASKS ####
+  ## Type of the mask worn by the workers (by default: "Surgical mask")
+  MaskType = "Surgical mask",
+  ## Acceptability by the workers for the different types of mask (proportion of the workers wearing it)
+  pMaskAcceptability = c("Surgical mask" = 0.8,
+                         "FFP2" = 0.60),
+  ## Mask efficiency for different droplet size classes [0 -> 1]
+  Mask_Eff = c(0.9,  
+               0.9,
+               0.9, 
+               0.9),
+
+  #### EPIDEMIOLOGY SCENARIO ####
+  ## The number of contaminated workers at the day 0
+  nContaminated_Init = 1,
+  ## Regional prevalence ##
+  prev = 50/100000
 )
